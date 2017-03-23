8f3e0feedfa1b3e555404f97403831d9dfc748d3
Justin van Elst <justinvanelst@gmail.com>
2017-03-23 16:22:45 +0100

    Revert "Update Product.php"
    This reverts commit e74aa71442c423263a069b8e2e6d3e84d2838328.

--- src/app/code/community/MagentoHackathon/FastSimpleExport/Model/Export/Entity/Category/Product.php
+++ src/app/code/community/MagentoHackathon/FastSimpleExport/Model/Export/Entity/Category/Product.php
@@ -5,7 +5,7 @@
  * Date: 10/25/14
  * Time: 11:54 AM
  */
-abstract class MagentoHackathon_FastSimpleExport_Model_Export_Entity_Category_Product extends Mage_ImportExport_Model_Export_Entity_Abstract
+class MagentoHackathon_FastSimpleExport_Model_Export_Entity_Category_Product extends Mage_ImportExport_Model_Export_Entity_Abstract
 {
 
     /**
